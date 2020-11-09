69a84f61990bc516d869574f539f798e2419156e
Lucas Angelo <lcs2001_@hotmail.com>
2020-11-09 20:17:01 -0300

    Em C

---
+++ C/cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C.exe"
+  "C.exe.manifest"
+  "C.pdb"
+  "CMakeFiles/C.dir/main.c.obj"
+  "libC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
