38088f73d661cf10b94e2ed7cdecef61e709f81c
Guizombas <guilherme.gabriel.532@gmail.com>
2020-12-09 16:18:39 -0300

    finalizado

--- perceptron.asm
+++ perceptron.asm
@@ -6,7 +6,12 @@
 	msg2:		.asciiz "\nDigite o numero de epocas que deseja treinar: "
 	msg3:		.asciiz "\nDigite o numero de dados para treinamento: "
 	msg4:		.asciiz "\nPesos iniciais: "
+	msg5:		.asciiz "\nNovos Pesos: "
+	msg6:		.asciiz "\nEtapa de Testes\n"
 	espaco:		.asciiz " "
+	barraEne:	.asciiz "\n"
+	mais:		.asciiz " + "
+	igual:		.asciiz " = "
 	preencheDados: 	.asciiz "\nDado: "
 	
 	#variaveis
@@ -42,9 +47,6 @@
 	addi $v0, $zero, 5
 	syscall
 	sw $v0, qtdEpocas
-	lw $a0, qtdEpocas # TESTE carregar a epoca no registrador f12 para imprimir
-	addi $v0, $zero, 1 # TESTE imprimir a epoca
-	syscall
 			
 	# Solicita quantidade de dados para treinar
 	addi $v0, $zero, 4
@@ -54,9 +56,6 @@
 	addi $v0, $zero, 5
 	syscall
 	sw $v0, qtdDados
-	lw $a0, qtdDados # TESTE carregar a qtdDados no registrador f12 para imprimir
-	addi $v0, $zero, 1 # TESTE imprimir a qtdDados
-	syscall
 			
 			
 	# Inicializa o FOR0
@@ -111,7 +110,8 @@
 	addi $v0, $zero, 2 
 	syscall
        
-       
+        # Carrega a taxa de aprendizado em $f3
+        lwc1 $f3, txAprendizado
         # Inicializa o FOR1
 	lw $s0, qtdEpocas
 	add $t0, $zero, $zero
@@ -133,11 +133,79 @@
                 	beq $t6, $zero, FIMFOR2
                 	# Controle da posição atual do vetor
                 	mul $t2, $t5, $t4
+                	
+               
+      			lw $s2, vetorDados($t2)
+      			mtc1 $s2, $f13
+  			cvt.s.w $f13, $f13
+      			lwc1 $f4, pesoUm
+      			lwc1 $f5, pesoDois
+      			
+      			#//funcao de ativacao
+      			#float res = pesoA*dados[j] + pesoB*dados[j] => dados[j] * (pesoA + pesoB)
+      			add.s $f0, $f4, $f5
+      			mul.s $f1, $f13, $f0
+      			
+      			# erro = (2*dados[j]) - res
+      			add.s $f0, $f13, $f13
+      			sub.s $f2, $f0, $f1
+      			
+      			
+      			#calculo dos novos pesos
+      			mul.s $f0, $f2, $f3
+      			mul.s $f0, $f0, $f13
+      			#pesoA += erro * taxa * dados[j]
+      			add.s $f4, $f4, $f0
+      			swc1 $f4, pesoUm
+      			#pesoB += erro * taxa * dados[j]
+      			add.s $f5, $f5, $f0
+      			swc1 $f5, pesoDois
+      			
+      			#printf("\n%d + %d = %.2f", dados[j], dados[j], res);
+      			# Print do resultado
+      			addi $v0, $zero, 4
+			la $a0, barraEne
+			syscall
+      			add $a0, $s2 ,$zero 
+			addi $v0, $zero, 1
+			syscall
+			
+      			addi $v0, $zero, 4
+			la $a0, mais
+			syscall
+			
+			add $a0, $s2 ,$zero 
+			addi $v0, $zero, 1
+			syscall
+			
+			addi $v0, $zero, 4
+			la $a0, igual
+			syscall
+			
+			sub.s $f11, $f11, $f11
+			add.s $f12, $f1, $f11
+			addi $v0, $zero, 2 
+			syscall
+			
+      			#printf("\nNovos pesos - A: %.2f, B: %.2f\n", pesoA, pesoB);
+      			# Mostra pesos novos do neuronio
+			addi $v0, $zero, 4
+			la $a0, msg5
+			syscall
+       
+        		add.s $f12, $f4, $f11
+			addi $v0, $zero, 2
+			syscall
+	
+			addi $v0, $zero, 4
+			la $a0, espaco
+			syscall
+	
+			add.s $f12, $f5, $f11
+			addi $v0, $zero, 2 
+			syscall
 				
-			# le e armazena o dado no vetor
-			#addi $v0, $zero, 5
-			#syscall
-			#sw $v0, vetorDados($t2)	
+				
 				
 			#i++
 			addi $t5, $t5, 1
@@ -150,28 +218,72 @@
                 j FOR1	
        FIMFOR1:
        
-       
-       
-       
-       
-       #depois resolve
+        addi $v0, $zero, 4
+	la $a0, msg6
+	syscall
+                 
+        #Pesos
+        lwc1 $f4, pesoUm
+        lwc1 $f5, pesoDois       
+        #inicializa FOR3                
+ 	lw $s0, qtdDados
+     	add $t0, $zero, $zero
+
+	# For para executar fase de testes
+        FOR3:
+        	# Condição de parada do FOR3
+                slt $t1, $t0, $s0
+       		beq $t1, $zero, FIMFOR3
+                
+             	# Solicita o dado atual do vetor de dados
+		addi $v0, $zero, 4
+		la $a0, preencheDados
+		syscall
+				
+		# le e armazena o dado no vetor
+		addi $v0, $zero, 5
+		syscall
+  		add $s2, $v0, $zero
+		mtc1 $s2, $f13
+  		cvt.s.w $f13, $f13
+		
+		#//funcao de ativacao
+      		#float res = pesoA*dados[j] + pesoB*dados[j] => dados[j] * (pesoA + pesoB)
+      		add.s $f0, $f4, $f5
+      		mul.s $f1, $f13, $f0
+      		
+      		#printf("\n%d + %d = %.2f", dados[j], dados[j], res);
+      		# Print do resultado
+      		addi $v0, $zero, 4
+		la $a0, barraEne
+		syscall
+      		add $a0, $s2 ,$zero 
+		addi $v0, $zero, 1
+		syscall
+			
+      		addi $v0, $zero, 4
+		la $a0, mais
+		syscall
+			
+		add $a0, $s2 ,$zero 
+		addi $v0, $zero, 1
+		syscall
+			
+		addi $v0, $zero, 4
+		la $a0, igual
+		syscall
+			
+		sub.s $f11, $f11, $f11
+		add.s $f12, $f1, $f11
+		addi $v0, $zero, 2 
+		syscall
+             	
+             	# i++
+                addi $t0, $t0, 1
+        	j FOR3
+	FIMFOR3:        
                         
                         
-     #            lw $s0, qtdDados
-      #      add $t0, $zero, $zero
-     #       addi $t4, $zero, 4
-#
-         #               FOR1:
-                 #               slt $t1, $t0, $s0
-       #                         beq $t1, $zero, FIMFOR1
-               #                 mul $t2, $t0, $t4
-             #   # le e armazena o dado no vetor
-             #   lw $a0, vetor($t2) # TESTE carregar a qtdDados no registrador f12 para imprimir
-            #    addi $v0, $zero, 1 # TESTE imprimir a qtdDados
-             #   syscall
-              #  addi $t0, $t0, 1
-                #                j FOR1
-                #        FIMFOR1:        
                         
                         
                         
@@ -181,6 +293,4 @@
                         
                         
                         
-                        
-                        
-                        jr $ra # finaliza
+	jr $ra # finaliza
